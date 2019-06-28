b50657562e17b2fac58d8ef1297f2148dcb827a0
azakenaton <luccho7.lh@gmail.com>
2019-06-28 11:50:45 +0200

    Erreur session

--- MESI-IPI/src/Controller/ConnexionController.php
+++ MESI-IPI/src/Controller/ConnexionController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
-session_start();
 
 class ConnexionController extends AbstractController
 {
